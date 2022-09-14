fa82aa43834fc44b52eb177b86e09dc55af5a788
ktlast <>
2022-09-15 00:07:52 +0800

    save drafts for Position and Portfolio class

--- crystal-dev/glance.cr
+++ crystal-dev/glance.cr
@@ -8,21 +8,53 @@ result_arr = JSON.parse(response.body)["msgArray"]
 p! result_arr.size
 
 result_arr.size.times { |x| 
+  puts result_arr[x]["c"] 
+  puts result_arr[x]["b"] 
   puts result_arr[x]["a"] 
 }
 
 
-class Portfolio
-  def initialize(stock_code : String, shares : Int32)
+class Position
+  # property stock_code : String
+  property shares : Int32
+  property at_cost : Float32
+
+  def initialize(stock_code : String)
     @stock_code = stock_code
-    @shares = shares
+    @shares = 0
+    @at_cost = 0.0
   end
 
   def go
-    puts "Code  Shares"
-    puts "#{@stock_code} #{@shares}"
+    puts "Code  #Shares   @cost"
+    puts "#{@stock_code} #{@shares}  #{@at_cost}"
   end
 end
 
+
+class Portfolio
+  # property positions : Hash(String, Position)
+  @positions : Hash(String, Position)
+  
+  def initialize()
+    @positions = Hash(String, Position).new
+  end
+
+  def add_position(position : Position)
+    @positions[position.stock_code] = position
+  end
+
+  def go
+    puts "Code  #Shares   @cost"
+    puts "#{@stock_code} #{@shares}  #{@at_cost}"
+  end
+end
+
+my_port = Portfolio.new
+pos_9933 = Position.new "9933"
+pos_9933.shares = 1000
+pos_9933.at_cost = 46.1
+my_port.add_position(pos_9933)
+my_port.go
 
 
