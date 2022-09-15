83fe6f1bbe0b04e50f2f537931bbb5c46b86c1df
ktlast <>
2022-09-15 09:28:06 +0800

    save: add quote class draft

--- crystal-dev/glance.cr
+++ crystal-dev/glance.cr
@@ -5,13 +5,31 @@ response = HTTP::Client.get "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?
 response.status_code      # => 200
 result_arr = JSON.parse(response.body)["msgArray"]
 
-p! result_arr.size
+#p! result_arr.size
 
-result_arr.size.times { |x| 
-  puts result_arr[x]["c"] 
-  puts result_arr[x]["b"] 
-  puts result_arr[x]["a"] 
-}
+#result_arr.size.times { |x| 
+#  puts result_arr[x]["c"] 
+#  puts result_arr[x]["b"] 
+#  puts result_arr[x]["a"] 
+#}
+
+
+class Quote
+  property quote_hash : Hash(String, Hash(String, String | Float32 | Int32))
+  @init_stock_code_array : Array(String)
+  
+  def initialize(init_stock_code_array)
+    @init_stock_code_array = init_stock_code_array
+    @quote_hash = Hash(String, Hash(String, String | Float32 | Int32)).new
+  end
+
+  def update_price()
+    @init_stock_code_array.size.times { |x|
+      puts @init_stock_code_array[x]
+    }
+
+  end  
+end
 
 
 class Position
@@ -39,11 +57,18 @@ class Portfolio
     @positions[position.stock_code] = position
   end
 
+  def update_quote()
+    puts ""
+  end
+
   def go
     puts "Code  #Shares   @cost"
     puts "#{@positions}"
   end
 end
+
+qq = Quote.new ["9933", "2330"]
+qq.update_price
 
 my_port = Portfolio.new
 pos_9933 = Position.new "9933"
@@ -51,5 +76,7 @@ pos_9933.shares = 1000
 pos_9933.at_cost = 46.1
 my_port.add_position(pos_9933)
 my_port.go
+
+
 
 
