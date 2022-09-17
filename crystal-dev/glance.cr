08e4bc23c8be687d0c2d6d3fe47ed766e1323997
ktlast <>
2022-09-17 13:44:41 +0800

    Add argument parser, and support reading portfolio csv now

--- crystal-dev/glance.cr
+++ crystal-dev/glance.cr
@@ -1,45 +1,13 @@
 require "http/client"
 require "json"
-require "colorize"
-
-
-def colored_string(input_num : Int32 | Float32, digits=3)
-  neg_flag = false
-  if input_num < 0 
-    neg_flag = true
-  end
-  whole_part = input_num.abs.to_i
-  whole_part_length = whole_part.to_s.size
-
-  if whole_part_length < digits
-    under_thousand_part = (whole_part.to_s.byte_slice(0, whole_part_length)) #.colorize.underline
-    if neg_flag
-      # p "[case 1] - input_num: #{input_num}, above: nil, under: #{under_thousand_part}"
-      return "-" + "#{under_thousand_part.colorize.underline}"
-    else
-      # p "[case 2] - input_num: #{input_num}, above: nil, under: #{under_thousand_part}"
-      return "#{under_thousand_part.colorize.underline}"
-    end
-    
-  else
-    above_thousand_part = (whole_part.to_s.byte_slice(0, whole_part_length-digits)) #.colorize.green
-    under_thousand_part = (whole_part.to_s.byte_slice(whole_part_length-digits, digits)) #.colorize.underline
-    if neg_flag
-      # p "[case 3] - input_num: #{input_num}, above: #{above_thousand_part}, under: #{under_thousand_part}"
-      return "#{("-"+above_thousand_part).colorize.green}\
-             #{under_thousand_part.colorize.underline}"
-    else
-      # p "[case 4] - input_num: #{input_num}, above: #{above_thousand_part}, under: #{under_thousand_part}"
-      return "#{above_thousand_part.colorize.magenta}"\
-             + "#{under_thousand_part.colorize.underline}"
-    end
-  end
-end
+require "csv"
+require "option_parser"
 
 
 class Portfolio
   # property exchange_table : Hash(String, String)
   # property positions : Hash(String, Hash(String, String | Int32 | Float32))
+  setter update_interval : Int32 = 15 
 
   def initialize()
     @exchange_table = Hash(String, String).new
@@ -47,16 +15,17 @@ class Portfolio
     @code_params = Set(String).new
     @total_pl = 0
     # @positions = Hash(String, Int32 | Hash(String, String)).new
-
-    File.read("../.exchange_table.csv").each_line do |line|
-      entry = line.split(",")
-      @exchange_table[entry[0]] = entry[1]
+    
+    CSV.each_row(File.read("../.exchange_table.csv")) do |row|
+      if row.size == 0
+        next
+      end
+      @exchange_table[row[0]] = row[1]
     end
   end
 
 
   def add_position(stock_code : String, shares : Int32, at_cost : Float32)
-    puts stock_code
     # puts "#{@exchange_table[stock_code]}_#{stock_code}.tw"
     @code_params.add("#{@exchange_table[stock_code]}_#{stock_code}.tw")
     @positions[stock_code] = {
@@ -81,8 +50,8 @@ class Portfolio
       #  puts  symbol["b"].to_s.split("_")[0].to_f32
       #  puts @positions[stock_code]["bid"] = symbol["b"]
       stock_code = symbol["c"]
-      bid_price = symbol["b"].to_s.split("_")[0].to_f32
-      ask_price = symbol["a"].to_s.split("_")[0].to_f32
+      bid_price = symbol["b"].to_s.split("_")[0].to_f32.round(2)
+      ask_price = symbol["a"].to_s.split("_")[0].to_f32.round(2)
       @positions[stock_code]["bid"] = bid_price
       @positions[stock_code]["ask"] = ask_price
       raw_pl = ((bid_price - @positions[stock_code]["at_cost"]) * @positions[stock_code]["shares"])
@@ -92,34 +61,21 @@ class Portfolio
   end
 
   def print_out()
-    # puts "#{"Code".ljust(8)}#{"# shares".rjust(9)}#{"@ Cost".rjust(11)}#{"Bid".rjust(10)}#{"Ask".rjust(10)}#{"P/L".rjust(12)}"
-    # @positions.keys.each do |stock_code|
-    #   entry_line = "#{stock_code.ljust(8)}\
-    #     #{colored_string(@positions[stock_code]["shares"]).rjust(9).ljust(15)}\
-    #     #{@positions[stock_code]["at_cost"].to_s.rjust(11)}\
-    #     #{@positions[stock_code]["bid"].to_s.rjust(10)}\
-    #     #{@positions[stock_code]["ask"].to_s.rjust(10)}\
-    #     #{colored_string(@positions[stock_code]["PL"]).rjust(25).ljust(25)}"
-    #   puts entry_line
-    # end
-    # puts "-"*60
-    # puts colored_string(@total_pl).rjust(60)
-
     # Title
-    puts "#{"Code".ljust(8)}#{"# Shares".rjust(9)}#{"@ Cost".rjust(11)}#{"Bid".rjust(10)}#{"Ask".rjust(10)}#{"$ P/L".rjust(12)}"
-    
+    puts "#{"Code".ljust(9)}#{"# Shares".rjust(10)}#{"@ Cost".rjust(13)}#{"*Bid".rjust(11)}#{"Ask".rjust(11)}#{"$ P/L".rjust(14)}"
+    puts "-"*68
     # entries
     @positions.keys.each do |stock_code|
-      entry_line = "#{stock_code.ljust(8)}\
-        #{@positions[stock_code]["shares"].format(",").rjust(9)}\
-        #{@positions[stock_code]["at_cost"].to_s.rjust(11)}\
-        #{@positions[stock_code]["bid"].to_s.rjust(10)}\
-        #{@positions[stock_code]["ask"].to_s.rjust(10)}\
-        #{@positions[stock_code]["PL"].format(",").rjust(12)}"
+      entry_line = "#{stock_code.ljust(9)}\
+        #{@positions[stock_code]["shares"].format(",").rjust(10)}\
+        #{@positions[stock_code]["at_cost"].to_s.rjust(13)}\
+        #{@positions[stock_code]["bid"].to_s.rjust(11)}\
+        #{@positions[stock_code]["ask"].to_s.rjust(11)}\
+        #{@positions[stock_code]["PL"].format(",").rjust(14)}"
       puts entry_line
     end
-    puts "-"*60
-    puts @total_pl.format(",").to_s.rjust(60)
+    puts "="*68
+    puts @total_pl.format(",").to_s.rjust(68)
   end
 
   def start()
@@ -127,15 +83,62 @@ class Portfolio
       puts "\33c\e[3J"
       self.update_quote
       self.print_out
-      sleep 30
+      sleep @update_interval
     end
   end
 end
 
-pp = Portfolio.new
-# puts pp.@exchange_table
-pp.add_position("9933", 4000, 46.1)
-pp.add_position("6248", 10000, 21.049)
-pp.start
+
+# Portfolio init
+my_portfolio = Portfolio.new
+portfolio_csv_path = "../portfolio.csv"
+
+
+# main argument parser
+option_parser = OptionParser.parse do |parser|
+  parser.banner = "Welcome to glance price on TW Stock !"
+
+  parser.on "-v", "--version", "Show version" do
+    puts "version: v0.0.1"
+    exit
+  end
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
+  end
+  parser.on "-i INTERVAL", "Set interval for updating quotes." do |interval|
+    my_portfolio.update_interval = interval.to_i
+  end
+  parser.on "-f PORTFOLIO_CSV", "Read PORTFOLIO_CSV to collect your Portfolio." do |portfolio_csv|
+    portfolio_csv_path = portfolio_csv
+  end
+  parser.missing_option do |option_flag|
+    STDERR.puts "ERROR: #{option_flag} is missing something."
+    STDERR.puts ""
+    STDERR.puts parser
+    exit(1)
+  end
+  parser.invalid_option do |option_flag|
+    STDERR.puts "ERROR: #{option_flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
+
+
+if ! File.exists?(portfolio_csv_path)
+  exit
+end  
+
+CSV.each_row(File.read(portfolio_csv_path)) do |row|
+  if row.size == 0 || row[0].strip.starts_with?('#')
+    next
+  end
+  my_portfolio.add_position(row[0], row[1].to_i, row[2].to_f32)
+end
+
+
+my_portfolio.start
+
 
 
