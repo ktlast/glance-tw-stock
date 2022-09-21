1bc32ba9f17bb05a20aeb0398c42bf4f8190eca7
ktlast <abc1238364@gmail.com>
2022-09-21 23:37:02 +0800

    CLI 版：現在會產生好看的 table (文字)、修正 README (#6)
    * remove org tables: those table will generate dynamically
    
    * ignore [.meta] and [.*.json]
    
    * Add [RemoteDataCache] class and add [add] and [delete] arg.
    1. The table-caching mechanism could be needed; and use this class to unify URL and File
    
    * ignore dwarf file and o file
    
    * Fix typing; fix bugs when adding and deleting position
    
    * debug init process when running in new env
    
    * prettify table
    
    * adjust comments for table output
    
    * update README: remove bash part and env table
    
    Co-authored-by: ktlast <>

--- README.md
+++ README.md
@@ -4,49 +4,21 @@
 ---
 ### Target Audience
 - 工程師：上班會需要用 command line 
-- 各行職員： 會需要用到表格 (google sheet)
+- 各行職員： 會需要用到 google 表格 (google sheet)
 
 但是
 - 每個人上班使用的作業系統不同 (Windows, OSX, Ubuntu .. )
 - 每家行號的網路環境也不盡相同，有些可以上網、有些有部分限制
 
-配合的解決方案參考下表：
-
-| 網路限制 \ OS   | Windows  | OSX | Linux | Linux (瀏覽器) |Docker | 
-|  ----  | ----  | ---- | ---- | ---- | ---- |
-| 完全能上網 | x | * | `Bash` | * | ** | 
-| 只能連 Google 相關服務  | `Web` | * | `Bash` | * | ** | 
-| 無法上網  | x | x | x | x | x | 
-
-其中
-- `*`：表示 Bash 版與 Web 版皆可取得報價
-- `Bash`：表示只能用 Bash 版取得報價
-- `Web`：表示只能用網頁版取得報價
-- `**`：docker 版只要能拉得到 image 就能執行。
 
 ---
 
-### A) 開發與環境說明
-- 預計會提供指令版 (zsh / bash) 與網頁版 (JS)，目前先暫以指令版著手開發。
-- bash 盡量使用 bash 4.0 以上版本，但指令都有盡量寫成通用的方式
-- 能用 zsh 就用 zsh，主要開發環境是 OSX - zsh
+### A) 安裝
+- 直接去 release 下載對應系統的執行檔。
 
 ---
 
-### B) 前置檢查
-- 需要有以下指令存在：
-
-```
-curl
-jq
-xargs
-cut
-python
-```
-
----
-
-### C) 說明 - 新增標的
+### B) 說明 - 新增標的
 
 1. 用 `add` 指令新增標的，標的格式為 `<股票代碼>_<股數>_<平均成本>`
 2. 輸入**股數**，而非張數；並用底線隔開
@@ -54,27 +26,32 @@ python
 
 ---
 
-### D) Example
+### C) Road Mapv
+- 預計會提供指令版與網頁版 (JS)，目前先暫以指令版著手開發。
+
+---
+
+### D) Example - CLI
 
 ```
 # 新增標的
-./glance-standalone.sh add 2330_10000_500
-./glance-standalone.sh add 2412_5000_125
-./glance-standalone.sh add 2498
+./glance add 2330,10000,500
+./glance add 2412,5000,kv125
+./glance add 2498
 
 
 # 如果要移除標的
-./glance-standalone.sh delete 2412
+./glance delete 2412
 
 
 # 查看目前有哪些標的
-./glance-standalone.sh list
+./glance list
 
 
 # 開始抓報價
-./glance-standalone.sh start
+./glance start
 
 
 # 修改更新週期 (秒)
-./glance-standalone.sh -i 10 start
+./glance -i 10 start
 ```
--- glance.cr
+++ glance.cr
@@ -4,42 +4,91 @@ require "csv"
 require "option_parser"
 
 
-class TableWatcher
-  # watch and download remote table periodically.
-  def initialize()
-    puts
-  end
-
-  def refresh_exchange()
-    exchange_hash = Hash(String, String).new
-    exchange_raw_csv = "https://raw.githubusercontent.com/ktlast/market-envs/master/stock-code-exchange.tw.csv"
-    response = HTTP::Client.get exchange_raw_csv
-
-    if response.status_code != "200"
-      puts "HTTP ERROR: got status code: [#{response.status_code}] (CSV URL: [#{exchange_raw_csv}])"
-      return exchange_hash
-    end
+# watch and download remote table periodically.
+class RemoteDataCache
+  def initialize(meta_cache_path : String)
+    @meta_cache_path = meta_cache_path
+    @meta_hash = Hash(String, Hash(String, Int64 | String)).new
     
-    CSV.each_row(response.body) do |row|
-      if row.size == 0
-        next
-      end
-      exchange_hash[row[0]] = row[1]
-      return exchange_hash
+    if File.exists?(@meta_cache_path)
+      @meta_hash = Hash(String, Hash(String, Int64 | String)).from_json(File.read(meta_cache_path))
+    else
+      File.write(@meta_cache_path, "{}")
     end
-  end 
-
-  def download_example_portfolio()
-    puts
   end
-end
+
+  def update_meta(meta_key, url=nil, file_suffix="txt")
+    if url == nil
+      puts "ERROR: input [url] is empty, please update with valid url. (meta file: [#{@meta_cache_path}])"
+      exit
+    end
+
+    # if @meta_hash.has_key?(meta_key)
+    #   url = @meta_hash[meta_key]["url"]
+    # else
+      
+    # end
+    @meta_hash[meta_key] = {
+      "timestamp_meta" => Time.utc.to_unix,
+      "timestamp_data" => Time.unix(0).to_unix,
+      "url" => url,
+      "file_path" => "./.meta.#{meta_key}.#{file_suffix}"
+    }
+    # File.write(@meta_hash[meta_key]["file_path"].to_s, "")
+    File.write(@meta_cache_path, @meta_hash.to_json)
+  end
+
+  def is_data_expired(meta_key, input_seconds)
+    return Time.utc.to_unix - @meta_hash[meta_key]["timestamp_data"].to_i > input_seconds
+  end
+
+  def download_data(meta_key)
+    response = HTTP::Client.get @meta_hash[meta_key]["url"].to_s
+    if response.status_code.to_s != "200"
+      puts "HTTP ERROR: got status code: [#{response.status_code}] (CSV URL: [#{@meta_hash[meta_key]["url"]}])"
+      return
+    end
+    File.write(@meta_hash[meta_key]["file_path"].to_s, response.body)
+    @meta_hash[meta_key]["timestamp_data"] = Time.utc.to_unix
+  end
+
+  def get_meta(meta_key, meta_type)
+    if ! @meta_hash.has_key?(meta_key)
+      puts "ERROR: [@meta_hash] does not have key: [#{meta_key}]"
+      return ""
+    end
+    return @meta_hash[meta_key][meta_type].to_s
+  end
+
+  def get_data(meta_key, refresh_only=false)
+    if ! @meta_hash.has_key?(meta_key)
+      puts "ERROR: [@meta_hash] does not have key: [#{meta_key}]"
+      return ""
+    end
+    if ! @meta_hash[meta_key]["url"].to_s.empty?
+      if self.is_data_expired(meta_key, 86400) || ! File.exists?(@meta_hash[meta_key]["file_path"].to_s)
+        self.download_data(meta_key)
+      end
+    end
+    if ! File.exists?(@meta_hash[meta_key]["file_path"].to_s)
+      puts "init file"
+      File.write(@meta_hash[meta_key]["file_path"].to_s, "")
+    end
+    if ! refresh_only
+      return File.read(@meta_hash[meta_key]["file_path"].to_s)
+    end
+    return ""
+
+  end
+end 
 
 
 
 class Portfolio
   # property exchange_table : Hash(String, String)
   # property positions : Hash(String, Hash(String, String | Int32 | Float32))
-  setter update_interval : Int32 = 15 
+  setter update_interval : Int32 = 15
+  @data_source = RemoteDataCache.new ".portfolio.meta.json"
 
   def initialize()
     @exchange_table = Hash(String, String).new
@@ -47,8 +96,11 @@ class Portfolio
     @code_params = Set(String).new
     @total_pl = 0
     # @positions = Hash(String, Int32 | Hash(String, String)).new
+
+    @data_source.update_meta("stock_exchange_table", "https://raw.githubusercontent.com/ktlast/market-envs/master/stock-code-exchange.tw.csv", file_suffix: "csv")
+    @data_source.update_meta("portfolio", "", file_suffix: "csv")
     
-    CSV.each_row(File.read("../.exchange_table.csv")) do |row|
+    CSV.each_row(@data_source.get_data("stock_exchange_table").to_s) do |row|
       if row.size == 0
         next
       end
@@ -57,22 +109,32 @@ class Portfolio
   end
 
 
-  def add_position(stock_code : String, shares : Int32, at_cost : Float32)
-    # puts "#{@exchange_table[stock_code]}_#{stock_code}.tw"
-    @code_params.add("#{@exchange_table[stock_code]}_#{stock_code}.tw")
-    @positions[stock_code] = {
-      "shares" => shares,
-      "at_cost" => at_cost,
-      "bid" => 0,
-      "ask" => 0,
-      "PL" => 0,
-    }
+  def update_position()
+    @code_params = Set(String).new
+    CSV.each_row(@data_source.get_data("portfolio")) do |row|
+      if row.size == 0
+        next
+      end
+      stock_code = row[0]
+      shares = row[1].to_i
+      at_cost = row[2].to_f32
+      
+      # puts "#{@exchange_table[stock_code]}_#{stock_code}.tw"
+      @code_params.add("#{@exchange_table[stock_code]}_#{stock_code}.tw")
+      @positions[stock_code] = {
+        "shares" => shares,
+        "at_cost" => at_cost,
+        "bid" => 0,
+        "ask" => 0,
+        "PL" => 0,
+      }
+    end
   end
 
-  def remove_position(stock_code : String)
-    @code_params.delete("#{@exchange_table[stock_code]}_#{stock_code}.tw")
-    @positions.delete(stock_code)
-  end
+  # def remove_position(stock_code : String)
+  #   @code_params.delete("#{@exchange_table[stock_code]}_#{stock_code}.tw")
+  #   @positions.delete(stock_code)
+  # end
 
   def update_quote()
     @total_pl = 0
@@ -93,26 +155,32 @@ class Portfolio
   end
 
   def print_out()
+    puts "╭#{"─" * 70}╮"
     # Title
-    puts "#{"Code".ljust(9)}#{"# Shares".rjust(10)}#{"@ Cost".rjust(13)}#{"*Bid".rjust(11)}#{"Ask".rjust(11)}#{"$ P/L".rjust(14)}"
-    puts "-"*68
+    puts "┊ #{"Code".ljust(9)}#{"# Shares".rjust(10)}#{"@ Cost".rjust(13)}#{"*Bid".rjust(11)}#{"Ask".rjust(11)}#{"$ P/L".rjust(14)} ┊"
+    # h-line
+    puts "╞#{"┄" * 70}╡"
     # entries
     @positions.keys.each do |stock_code|
-      entry_line = "#{stock_code.ljust(9)}\
+      entry_line = "┊ #{stock_code.ljust(9)}\
         #{@positions[stock_code]["shares"].format(",").rjust(10)}\
         #{@positions[stock_code]["at_cost"].to_s.rjust(13)}\
         #{@positions[stock_code]["bid"].to_s.rjust(11)}\
         #{@positions[stock_code]["ask"].to_s.rjust(11)}\
-        #{@positions[stock_code]["PL"].format(",").rjust(14)}"
+        #{@positions[stock_code]["PL"].format(",").rjust(14)} ┊"
       puts entry_line
     end
-    puts "="*68
-    puts @total_pl.format(",").to_s.rjust(68)
+    # h-line for total PL
+    puts "╞#{"=" * 70}╡"
+    # total PL
+    puts "┊ TOTAL: #{@total_pl.format(",").to_s.rjust(61)} ┊"
+    puts "╰#{"─" * 70}╯"
   end
 
   def watch_forever()
     while true
       puts "\33c\e[3J"
+      self.update_position
       self.update_quote
       self.print_out
       sleep @update_interval
@@ -122,34 +190,68 @@ end
 
 
 # Portfolio init
+# data_source = RemoteDataCache.new "deleteme.meta.json"
+
+
+# p data_source.get_meta("stock_exchange_table", "file_path").to_s
+
+# data_source.get_data("stock_exchange_table", refresh_only=true)
+portfolio_data_source = RemoteDataCache.new ".portfolio.meta.json"
+
 my_portfolio = Portfolio.new
-portfolio_csv_path = "../portfolio.csv"
+# my_portfolio = Portfolio.new data_source.get_meta("stock_exchange_table", "file_path").to_s
 
 
 # main argument parser
 option_parser = OptionParser.parse do |parser|
-  parser.banner = "Welcome to glance price on TW Stock !"
+  parser.banner = "Welcome to glance price on TW Stock !\n"
 
   parser.on "-v", "--version", "Show version" do
     puts "version: v0.0.1"
     exit
   end
+
   parser.on "-h", "--help", "Show help" do
     puts parser
     exit
   end
+
+  parser.on "-a POSITION", "Add a Position entry. format: <code>,<shares>,<at_cost>" do |position|
+    content = portfolio_data_source.get_data("portfolio").to_s
+    input_pos = position.split(",")
+    if input_pos.size != 3
+      position = "#{input_pos[0]},0,0"
+    end
+    File.write(portfolio_data_source.get_meta("portfolio", "file_path").to_s, (content.gsub(/#{input_pos[0]},[\.0-9,]+\n?/, "") + "\n#{position}").gsub(/\n\n+/, "\n"))
+    exit
+  end
+
+  parser.on "-d STOCK_CODE", "Delete a Position entry. format: <code>" do |stock_code|
+    content = portfolio_data_source.get_data("portfolio").to_s
+    File.write(portfolio_data_source.get_meta("portfolio", "file_path").to_s, content.gsub(/#{stock_code}[\.0-9,]*\n?/, "").gsub(/\n\n+/, "\n"))
+    exit
+  end
+
+  parser.on "-l", "List Positions." do
+    puts File.read(portfolio_data_source.get_meta("portfolio", "file_path").to_s)
+    exit
+  end
+
   parser.on "-i INTERVAL", "Set interval for updating quotes." do |interval|
     my_portfolio.update_interval = interval.to_i
   end
-  parser.on "-f PORTFOLIO_CSV", "Read PORTFOLIO_CSV to collect your Portfolio." do |portfolio_csv|
-    portfolio_csv_path = portfolio_csv
+  
+  parser.on "start", "Start update quotes forever based on portfolio." do
+    my_portfolio.watch_forever
   end
+
   parser.missing_option do |option_flag|
     STDERR.puts "ERROR: #{option_flag} is missing something."
     STDERR.puts ""
     STDERR.puts parser
     exit(1)
   end
+
   parser.invalid_option do |option_flag|
     STDERR.puts "ERROR: #{option_flag} is not a valid option."
     STDERR.puts parser
@@ -158,16 +260,15 @@ option_parser = OptionParser.parse do |parser|
 end
 
 
-if ! File.exists?(portfolio_csv_path)
-  exit
-end  
 
-CSV.each_row(File.read(portfolio_csv_path)) do |row|
-  if row.size == 0 || row[0].strip.starts_with?('#')
-    next
-  end
-  my_portfolio.add_position(row[0], row[1].to_i, row[2].to_f32)
-end
+# CSV.each_row(data_source.get_data("portfolio")) do |row|
+#   if row.size == 0 || row[0].strip.starts_with?('#')
+#     next
+#   end
+ 
+# end
+
+# my_portfolio.watch_forever
 
 
 
