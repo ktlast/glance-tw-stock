842085accedbac8fd9d823dc43f02e15f63fa570
ktlast <>
2023-04-24 11:03:47 +0800

    draft init

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,38 @@
-// struct Position {
-//     symbol_code: String,
-//     share: i32,
-//     at_cost: u16
-// }
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
-use std::collections::HashMap;
+fn main() {
+    println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-// fn fetch_symbols() {
-    // }
+    // println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
     
-// fn main() {
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::blocking::get("https://httpbin.org/ip")?
-        .json::<HashMap<String, String>>()?;
-    println!("{:#?}", resp);
-    Ok(())
 }
-
-
-
-
